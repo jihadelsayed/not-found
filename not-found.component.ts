fc5a9cd913babcff8c8c5b76c5b528a76dacdd2d
Jihad Sayed <david.pales1@gmail.com>
2025-05-09 09:51:14 -0500

    Update not-found.component.ts

--- not-found.component.ts
+++ not-found.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgClass } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 @Component({
+  standalone: true,
+  imports: [RouterModule], 
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrls: ['./not-found.component.scss']
